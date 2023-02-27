526b7880b741778e7d7fe923c4e9cf4e5868bfd8
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-02-27 12:56:37 +0200

    Updated changes to interactive component

--- README.md
+++ README.md
@@ -17,3 +17,18 @@
 
   We appreciate you taking the time to give a rating. If you ever need more support, 
   don’t hesitate to get in touch!
+
+  ### Neutral
+
+- White: hsl(0, 0%, 100%)
+- Light Grey: hsl(217, 12%, 63%)
+- Medium Grey: hsl(216, 12%, 54%)
+- Dark Blue: hsl(213, 19%, 18%)
+- Very Dark Blue: hsl(216, 12%, 8%)
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,37 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
+// import
+import Star from "../src/assets/images/icon-star.svg";
+import ThankYou from "../src/assets/images/illustration-thank-you.svg";
+
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="container">
+        <div className="card__contentWrapper">
+          {/* <div className="card__starWrapper"> */}
+          <img src={Star} alt="Star icon" className="card__star" />
+          {/* </div> */}
+          <h2 className="card__title">How did we do?</h2>
+          <p className="card__para">
+            Please let us know how we did with your support request. All
+            feedback is appreciated to help us improve our offering!
+          </p>
+          <div className="card__ratingWrapper">
+            <p className="card__ratingItem">1</p>
+            <p className="card__ratingItem">2</p>
+            <p className="card__ratingItem">3</p>
+            <p className="card__ratingItem">4</p>
+            <p className="card__ratingItem">5</p>
+          </div>
+          <div className="card__btnWrapper">
+            <a href="#" className="card__btn">
+              Submit
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
